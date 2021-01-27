5677091e7963a57933b5d014b4970d6f6554b680
selsmie <simonelsmie@hotmail.com>
2021-01-27 13:51:51 +0000

    files set up

---
+++ app/models/order_list.py
@@ -0,0 +1,5 @@
+from app.models.order import *
+
+order1 = Order(self, customer_name, order_date, quantity, book_title, value)
+order2 = Order(self, customer_name, order_date, quantity, book_title, value)
+orders = [order1, order2]
